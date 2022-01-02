9d46d65b41e268597c9a12d031db8e4a44a30e52
David Tu <freshingmoon0725@gmail.com>
2022-01-02 20:58:52 +0800

    impl functions peristant

---
+++ free_marker/sample/functions/src/func/autoIncrementNumber/index.js
@@ -0,0 +1,19 @@
+import {
+  utiller as Util,
+  exceptioner as ERROR,
+  pooller as InfinitePool,
+} from "utiller";
+import _ from "lodash";
+import libpath from "path";
+import BaseAutoIncrementNumber from "./BaseAutoIncrementNumber";
+
+class AutoIncrementNumber extends BaseAutoIncrementNumber {
+  /** -------------------- fields -------------------- **/
+  /** -------------------- functions -------------------- **/
+
+  constructor(props) {
+    super(props);
+  }
+  /** -------------------- async api -------------------- **/
+}
+export default new AutoIncrementNumber();
