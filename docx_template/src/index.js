3c9de4ca40e7447b513b6bd2db6586fa078feea2
David Tu <freshingmoon0725@gmail.com>
2024-07-09 22:57:35 +0800

    實作docx產生器

---
+++ docx_template/src/index.js
@@ -0,0 +1,51 @@
+import {configerer} from "configerer";
+import {utiller as Util, exceptioner as ERROR, pooller as InfinitePool} from 'utiller';
+import _ from 'lodash';
+import libpath from 'path';
+import Moment from 'moment';
+import * as fs from 'fs';
+
+/** author:明悅
+ *  create time:Sun Jul 07 2024 16:01:51 GMT+0800 (Taipei Standard Time)
+ */
+
+class docx_template {
+
+}
+
+export {docx_template as docx_template}
+
+if (configerer.DEBUG_MODE) {
+    (async () => {
+            const PizZip = require("pizzip");
+            const Docxtemplater = require("docxtemplater");
+
+// Load the docx file as binary content
+            const content = fs.readFileSync(`./template_of_dading_contract.docx`);
+// Unzip the content of the file
+            const zip = new PizZip(content);
+// This will parse the template, and will throw an error if the template is
+// invalid, for example, if the template is "{user" (no closing tag)
+            const doc = new Docxtemplater(zip, {
+                paragraphLoop: true,
+                linebreaks: true,
+            });
+// Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
+            doc.render({
+                nameOfTravel: "台中3日行",
+            startDateOfTravel : "12月2號",
+                countsOfPeople: "6",
+            });
+// Get the zip document and generate it as a nodebuffer
+            const buf = doc.getZip().generate({
+                type: "nodebuffer",
+                // compression: DEFLATE adds a compression step.
+                // For a 50MB output document, expect 500ms additional CPU time
+                compression: "DEFLATE",
+            });
+// buf is a nodejs Buffer, you can either write it to a
+// file or res.send it with express for example.
+            fs.writeFileSync(`./output${_.toString(Util.getCurrentTimeStamp())}.docx`, buf);
+        }
+    )();
+}
---
+++ free_marker/project-dading/admin/src/index.js
@@ -0,0 +1,110 @@
+const edit = true;
+import Api from './api';
+import {databazer as Databaser, builder as Builder} from "databazer";
+import {utiller as Util, pooller as InfinitePool, exceptioner as ERROR} from "utiller";
+import _ from 'lodash';
+import Listener from './listener'
+import firebase from "./base/CommonFirebaseHelper";
+import {linepayer as LinePay} from "linepayer";
+import libpath from 'path';
+import config from './config';
+import moment from 'moment';
+import fs from 'fs';
+import PizZip from 'pizzip';
+import Docxtemplater from 'docxtemplater';
+import { load } from '@pspdfkit/nodejs'
+
+
+(async () => {
+
+    // console.log(firebase.storage().bucket());
+
+    async function deployDocxFileToAdminStorage(buffer, fileName = 'folder/filename.extension') {
+        if (!fileName.endsWith('.docx')) {
+            return {
+                succeed: false,
+                message: `檔案產生失敗，原因：副檔名不是.docx`
+            }
+        }
+        return await deployButterAsFile2AdminStorage(buffer, fileName, `application/vnd.openxmlformats-officedocument.wordprocessingml.document`)
+    }
+
+    async function deployPDFtoAdminStorage(buffer, fileName = 'folder/filename.extension') {
+        if (!fileName.endsWith(`.pdf`)) {
+            return {
+                succeed: false,
+                message: `檔案產生失敗，原因：副檔名不是.pdf`
+            }
+        }
+
+        return await deployButterAsFile2AdminStorage(buffer, fileName, `application/pdf`)
+    }
+
+    async function deployButterAsFile2AdminStorage(buffer, fileName, contentType) {
+        const ref = firebase.storage().bucket()
+        const core = ref.file(fileName);
+        try {
+            await core.save(buffer, {contentType});
+            // console.log('File uploaded successfully:', result.metadata);
+
+            const downloadUrl = await core.getSignedUrl({
+                action: "read",
+                expires: "03-09-3000",
+            })
+            return {
+                succeed: true,
+                path: downloadUrl,
+                message: `produce doc file succeed`
+            }
+        } catch (error) {
+            return {
+                succeed: false,
+                message: `檔案產生失敗，原因：${error.message}`
+            }
+        }
+    }
+
+    async function getBufferOfGeneratedDocx(pathOfDocxTemplate = {}, data = {nameOfTravel: "小卉國8日行", startDateOfTravel: "2月17號", countsOfPeople: "2"}) {
+        /** Load the docx file as binary content */
+        const content = fs.readFileSync(pathOfDocxTemplate);
+        /** Unzip the content of the file */
+        const zip = new PizZip(content);
+        /** This will parse the template, and will throw an error if the template is
+         invalid, for example, if the template is "{user" (no closing tag) */
+        const doc = new Docxtemplater(zip, {
+            paragraphLoop: true,
+            linebreaks: true,
+        });
+        /** Render the document (Replace {first_name} by John, {last _name} by Doe, ...) */
+        doc.render(data);
+        /** Get the zip document and generate it as a nodebuffer */
+        const buf = doc.getZip().generate({
+            type: "nodebuffer",
+            /** compression: DEFLATE adds a compression step.
+             For a 50MB output document, expect 500ms additional CPU time */
+            compression: "DEFLATE",
+        });
+        /** doing something of log usage,buf is a node.js Buffer, you can either write it to a file or res.
+         * persist file will => fs.writeFileSync(`./output${_.toString(Util.getCurrentTimeStamp())}.docx`, buf); */
+        return buf;
+
+    }
+
+    async function getBufferOfDocx2PDF(bufferOfDocx) {
+        const instance = await load({document: bufferOfDocx})
+        const pdfBuffer = await instance.exportPDF();
+        await instance.close();
+        return Buffer.from(pdfBuffer);
+    }
+
+
+    const bufferOfDocx = await getBufferOfGeneratedDocx(`./template_of_dading_contract_0707.docx`, {nameOfTravel: "小卉國8日行", startDateOfTravel: "2月17號", countsOfPeople: "2"})
+    const bufferOfPDF = await getBufferOfDocx2PDF(bufferOfDocx);
+    // const result = await deployPDFtoAdminStorage(bufferOfPDF, libpath.join('contract', `大鼎${_.toString(Util.getCurrentTimeStamp())}.pdf`));
+
+    const result = await deployDocxFileToAdminStorage(bufferOfDocx, libpath.join('contract', `大鼎${_.toString(Util.getCurrentTimeStamp())}.docx`));
+    console.log(result);
+
+})();
+
+
