8a6102db3ed66f00996e931fe87b00280761a9c1
David Tu <freshingmoon0725@gmail.com>
2024-10-13 16:28:50 +0800

    增加sashascraper

--- sashanail_scapier/src/index.js
+++ sashanail_scapier/src/index.js
@@ -3,6 +3,7 @@ import {utiller as Util, exceptioner as ERROR, pooller as InfinitePool } from 'u
 import _ from 'lodash';
 import libpath from 'path';
 import Moment from 'moment';
+import puppeteer from 'puppeteer';
 
 /** author:明悅
  *  create time:Sun Oct 13 2024 02:27:45 GMT+0800 (Taipei Standard Time)
@@ -16,6 +17,12 @@ export { sashanail_scapier as sashanail_scapier }
 
 if (configerer.DEBUG_MODE) {
 (async () => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+
+        const page = await browser.newPage();
+        await page.goto(`https://www.sachianail.com/plist/-1`, {waitUntil: 'networkidle2'});
 
 
     }
