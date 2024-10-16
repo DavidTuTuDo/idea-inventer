6cea3a1e197f52e52e56b478e1b427cf4629e05d
David Tu <freshingmoon0725@gmail.com>
2024-10-16 20:49:59 +0800

    增加sasha的爬蟲機制

--- sashanail_scapier/src/index.js
+++ sashanail_scapier/src/index.js
@@ -11,6 +11,115 @@ import puppeteer from 'puppeteer';
 
 class sashanailgel_scraper {
 
+    constructor(engine) {
+        this.browser = engine;
+    }
+
+
+    async fetchListOfTitleHref() {
+        const pages = [];
+        const page = await this.browser.newPage();
+        await page.goto(`https://www.sachianail.com/`, {waitUntil: 'networkidle2', timeout: 0});
+        const rowElement = await page.$$('#mTopBar > *');
+        console.log(_.size(rowElement));
+        for (const each of rowElement) {
+            const barElement = await each.$('.mbcUshop-firstLvBarItem > a');  // 获取父元素
+            const titleText = await barElement.evaluate(el => {
+                return {href: el.href, title: el.innerText}
+            });
+            pages.push(titleText);
+        }
+        console.log(pages);
+        return pages;
+    }
+
+    // 滚动页面并检查是否有新的内容加载
+    async scrollToBottomAndCheck(page) {
+        const delay = 1000; // 等待新内容加载的延迟时间
+        let lastHeight = await page.evaluate('document.body.scrollHeight');
+
+        while (true) {
+            await page.evaluate('window.scrollBy(0, document.body.scrollHeight)');
+
+            // 等待新的内容加载
+            await new Promise(resolve => setTimeout(resolve, delay));
+
+            let newHeight = await page.evaluate('document.body.scrollHeight');
+
+            // 如果页面高度不再变化，表示没有新的内容加载，退出循环
+            if (newHeight === lastHeight) {
+                console.log("No more new content, closing page...");
+                break;
+            }
+            lastHeight = newHeight;
+        }
+    }
+
+    async fetchWholePageProduct(path) {
+        const page = await this.browser.newPage();
+        await page.goto(path.href, {waitUntil: 'networkidle2', timeout: 0});
+
+        /**
+         * 一些puppeteer使用的心得！！
+         * .$eval = evaluate :取得element元素底下的資料
+         * $ 是拿到 reference
+         * $$ 是拿到 element list
+         * sample  const items = await page.$$eval('#gl-container > *', elements => {
+         * sample  const titles = await page.$$eval('#gl-container .gl-title', elements => {
+         * '#'=>代表id | '.'=>代表class | <tag不用加前綴  #id > .className > tag
+         */
+        const subtitles = [];
+        await Util.syncDelay(1000);
+        const listOfSubTitles = await page.$$('#mTopSubBar > .m-list-sub-wrap > .list-row-sub-container > ul > *');
+        console.log(path.title, _.size(listOfSubTitles));
+        if (_.size(listOfSubTitles) > 0) {
+            for (const subtitle of listOfSubTitles) {
+                const subtitleElement = await subtitle.$('li > a');
+                const objectOfSubtitle = await subtitleElement.evaluate(el => {
+                    return {href: el.href, title: el.innerText}
+                });
+                subtitles.push(objectOfSubtitle);
+                console.log(`${path.title} 底下有`, subtitles)
+            }
+        } else {
+            console.log(`${path.title} 底下沒有sub title`)
+        }
+        await page.close()
+        return;
+        await this.scrollToBottomAndCheck(page);
+        await Util.syncDelay(3000);
+
+        const parentElement = await page.$$('#gl-container > *');  // 获取父元素
+        // console.log('parent => ', parentElement);
+        const items = {};
+
+        for (const row of parentElement) {
+            const rowElement = await page.$$('.divFormProductListItem');
+            // console.log('row => ', row, _.indexOf(parentElement, row));
+            for (const each of rowElement) {
+                const titleElement = await each.$('.gl-title');
+                const titleText = await titleElement.evaluate(el => el.innerText);
+                // console.log(titleText);
+                const hrefOfDetailElement = await each.$('.gl-img > .gl-item-image > .img-link')
+                const srcValue = await hrefOfDetailElement.evaluate(el => el.href);  // 或者 el.getAttribute('src') 来更精确获取原始值
+                const subs = []
+                const subItemOptionElement = await each.$$('.addon-select option');
+                for (const items of subItemOptionElement) {
+                    subs.push(await items.evaluate(el => {
+                        if (el.value && parseInt(el.value) > 0)
+                            return {name: el.innerText, value: el.value}
+                    }))
+                }
+                // console.log(subs);
+                items[srcValue] = {name: titleText, subs: _.filter(subs, sub => !_.isUndefined(sub)), href: srcValue};
+            }
+        }
+
+        let products = _.values(items);
+        console.log('所有商品數量：', _.size(products), '商品底下所有種類合計：', _.sum(products.map(item => _.size(item.subs))));
+        await Util.persistJsonFilePrettier(`./products.json`, products);
+        await page.close();
+    }
 }
 
 export {sashanailgel_scraper as sashanailgel_scraper}
@@ -21,48 +130,22 @@ if (configerer.DEBUG_MODE) {
                 headless: false
             });
 
-            const page = await browser.newPage();
-            await page.goto(`https://www.sachianail.com/plist/-1`, {waitUntil: 'networkidle2', timeout: 0});
-            //   const items = await page.$$eval('#gl-container > *', elements => {
-            //   const titles = await page.$$eval('#gl-container .gl-title', elements => {
-            // '#'=>代表id '.'=>代表class
-            // .$eval = evaluate
-            // $ 是拿到 reference
-            // $$ 是拿到 element list
-
-
-            /**
-             const parentElement = await page.$('#gl-container');  // 获取父元素
-             */
-
-            const parentElement = await page.$$('#gl-container > *');  // 获取父元素
-            for (const each of parentElement) {
-                const titleElement = await each.$('.gl-title');
-                const titleText = await titleElement.evaluate(el => el.innerText);
-                console.log(titleText);
-
-                const hrefOfDetailElement = await each.$('gl-img > gl-item-image > img-link')
-                const srcValue = await hrefOfDetailElement.evaluate(el => el.href);  // 或者 el.getAttribute('src') 来更精确获取原始值
-                console.log(srcValue);
-
+            const handler = new sashanailgel_scraper(browser);
+            const pages = await handler.fetchListOfTitleHref();
+            const pagesShouldFetch = pages.filter(page => {
+                const splits = page.href.split('/');
+                const id = splits.pop();
+                const path = splits.pop();
+                return id > 0 && _.isEqual(path, 'plist');
+            })
+            console.log(pagesShouldFetch);
+            for (const page of pagesShouldFetch) {
+                await handler.fetchWholePageProduct(page)
             }
-            // console.log(parentElement[0]);
 
-            // 获取父元素下class="gl-title"的子元素
-            // const titleElements = await parentElement.$$('.gl-title'); // 找到所有子元素class="gl-title"
-            //
-            // // 遍历这些子元素并获取文本
-            // for (let element of titleElements) {
-            //     const text = await element.$eval(el => el.innerText);  // 也可以使用el.innerHTML或其他属性
-            //     console.log(text);
-            // }
 
-            //     , elements => {
-            //     // 将所有子元素的文本或其他属性提取出来
-            //     return elements.map(element => {
-            //         return {name:element.$$eval('gl-title')}
-            //     }); // 或者 element.outerHTML 获取HTML内容
-            // });
+            // pagesShouldFetch.map(page => handler.fetchWholePageProduct(page).then(result => result))
+            // const products = pagesShouldFetch.map(page =>  fetchWholePageProduct(page))
 
         }
     )();
